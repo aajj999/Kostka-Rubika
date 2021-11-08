a247e587fc821c46960f2f4d39ed5bef9ebf0ad3
Ania <jablonowska999@gmail.com>
2021-11-08 12:12:28 +0100

    Template for tests added

---
+++ src/concurrentcube/CubeTest.java
@@ -0,0 +1,14 @@
+package concurrentcube;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CubeTest {
+
+    @org.junit.jupiter.api.Test
+    void rotate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void show() {
+    }
+}
